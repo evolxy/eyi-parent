a051247c9167936236a41987a5ebac02ae911f95
夜静月明 <=xu.yupeng@qq.com>
2022-03-14 11:29:47 +0800

    版本管理

---
+++ eyi-commons/src/test/java/com/xu/commons/EyiCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xu.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyiCommonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
