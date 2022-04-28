1061df682ad7c0f1dfd6ccdd5070ab6ae416eb4e
夜静月明 <=xu.yupeng@qq.com>
2022-04-28 17:05:49 +0800

    添加RestTemplate配置

--- eyi-commons/src/main/java/com/xu/commons/utils/TikaUtils.java
+++ eyi-commons/src/main/java/com/xu/commons/utils/TikaUtils.java
@@ -1,21 +1,38 @@
 package com.xu.commons.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tika.Tika;
+import org.apache.tika.mime.MimeType;
+import org.apache.tika.mime.MimeTypeException;
+import org.apache.tika.mime.MimeTypes;
 
 /**
  * @author Author
  * @version 0.1
  * Created On 2022/4/19 11:00
  */
-
+@Slf4j
 public class TikaUtils {
-    public static String getContentTypeByExtension(String extension) {
-        Tika tika = new Tika();
-        return tika.detect("test." + extension);
-    }
+	public static String getContentTypeByExtension(String extension) {
+		Tika tika = new Tika();
+		return tika.detect("test." + extension);
+	}
 
-    public static String getContentTypeByFileName(String filename) {
-        Tika tika = new Tika();
-        return tika.detect(filename);
-    }
+	public static String getContentTypeByFileName(String filename) {
+		Tika tika = new Tika();
+		return tika.detect(filename);
+	}
+
+	public static String getExtByContentType(String contentType) {
+		MimeTypes all = MimeTypes.getDefaultMimeTypes();
+		MimeType type;
+		String res = "";
+		try {
+			type = all.forName(contentType);
+			res = type.getExtension();
+		} catch (MimeTypeException e) {
+			log.error(e.getMessage(), e);
+		}
+		return res;
+	}
 }
